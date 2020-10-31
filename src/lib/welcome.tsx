bf0112e04a50432febcbd63c873e26e68fd35a39
7korobi <7korobi@gmail.com>
2020-11-01 03:57:57 +0900

    fix for dais oldlog

--- src/lib/welcome.tsx
+++ src/lib/welcome.tsx
@@ -20,23 +20,23 @@ export function Welcome() {
   const folder =
     Query.folders.where((o)=> o.match(location.href) ).list.head || Query.folders.find('DAIS')
 
-  const isSimple = !document.title.match(/トップページ|終了済みの村の一覧/)
-  const isFull = !isSimple
-  const { isOldLog } = folder
+  const isTop = !!document.title.match(/トップページ/)
+  const isOldLog = !!document.title.match(/終了済みの村の一覧/)
+  const isFull = (isTop || isOldLog)
 
   const chats = (
     Query.phases.where({ folder_id: folder.id }).list.head || Query.phases.find('BRAID-top-0-0')
   ).chats.list
 
   let toppage = location.href.replace(location.search, '').replace(location.hash, '')
-  if (isOldLog) {
+  if (folder.isOldLog) {
     toppage += "?cmd=oldlog"
   } 
 
   return (
     <div>
       <div id="welcome" style={style}>
-        {isSimple || <Export />}
+        {isFull && <Export />}
         <h1 className="title-bar">
           <a dangerouslySetInnerHTML={{ __html: folder?.title }} href={toppage}></a>
         </h1>
@@ -52,7 +52,7 @@ export function Welcome() {
           </div>
         </div>
       </div>
-      {isSimple || isOldLog || (
+      {isTop && (
         <div className="outframe">
           <div className="contentframe">
             <div className="inframe">
